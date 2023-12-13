ffeeeecc1d9bd1634be59c971791ee1de613b4de
mlinde <mlinde@utu.fi>
2023-12-13 11:58:10 +0200

    Edited function names and removed return statements

--- R/violinPlot.R
+++ R/violinPlot.R
@@ -1,31 +1,28 @@
 # Plots violin plot of features grouped by clusters
 
-CellRomeR.violin <- function(clusters, features) {
+plotViolin <- function(clusters, features) {
   violin_data <- data.frame(
     cluster = clusters,
     feature = features
   )
   
   
-  p <- ggplot(violin_data, aes(x=cluster, y=feature, fill=cluster)) +
+  ggplot(violin_data, aes(x=cluster, y=feature, fill=cluster)) +
     geom_violin() + theme_classic()
   
-  return(p)
   
 }
 
 # Plots violin plot of features grouped by clusters horizontally
-CellRomeR.hviolin <- function(clusters, features) {
+plotHviolin <- function(clusters, features) {
   violin_data <- data.frame(
     cluster = clusters,
     feature = features
   )
   
   
-  p <- ggplot(violin_data, aes(x=cluster, y=feature, fill=cluster)) +
+  ggplot(violin_data, aes(x=cluster, y=feature, fill=cluster)) +
     geom_violin() + theme_classic() + coord_flip()
   
-  
-  return(p)
   
 }
