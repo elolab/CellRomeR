942dd544da686f19aec57ead0de33335a6d32561
Tomi Suomi <tomi.suomi@utu.fi>
2023-12-14 17:35:07 +0200

    Fix generic S3 plot

--- R/plot.R
+++ R/plot.R
@@ -3,16 +3,16 @@ plot.MigrDat <- function(x, ...) {
   n <- length(unique(x@clustering$S$ILoRegclusters))
   colors <- colorRampPalette(c("#3C95AC","#E756A7","#FFCB8A"))(n)[x@clustering$S$ILoRegclusters]
   
-  x <- x@spots$raw$POSITION_X
-  y <- x@spots$raw$POSITION_Y
-  plot(x, y, pch=NA, ...)
+  x.coords <- x@spots$raw$POSITION_X
+  y.coords <- x@spots$raw$POSITION_Y
+  plot(x.coords, y.coords, pch=NA, ...)
   
   ord <- order(x@spots$raw$POSITION_T, decreasing=FALSE)
   for(track in unique(x@spots$raw$TRACK_ID)) {
     sel <- which(x@spots$raw$TRACK_ID[ord]==track)
-    lines(x[ord][sel], y[ord][sel], col="lightgrey")
+    lines(x.coords[ord][sel], y.coords[ord][sel], col="lightgrey")
   }
   
-  points(x, y, pch=21, bg=colors[ord], cex=0.5)
+  points(x.coords, y.coords, pch=21, bg=colors[ord], cex=0.5)
   
 }
