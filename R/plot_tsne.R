8b9d0adf57942cc02534273463ed7340ad937d2d
Tomi Suomi <tomi.suomi@utu.fi>
2023-12-14 10:49:56 +0200

    Update tSNE

--- R/plot_tsne.R
+++ R/plot_tsne.R
@@ -1,7 +1,13 @@
-plot_tsne <- function(MigrObj, type, uniq, colors) {
-  clusters <- clusterings(MigrDatXML)[["S"]]$ILoRegclusters
-  tsne <- MigrDatXML@dimreductions[["S"]][[paste("CH1_15", "_TSNE")]] 
-  tsne.x = tsne[, 1]
-  tsne.y = tsne[, 2]
-  plot(tsne.x, tsne.y, bty="n", pch=19, cex=0.1, col=color_clusters[clusters], axes=FALSE, xlab="", ylab="")
+plot_tsne <- function(MigrObj, uniq=NULL) {
+  
+  if (is.null(uniq)) {
+    stop("Please specify a named clustering")
+  }
+  
+  clusters <- clusterings(MigrObj)[["S"]]$ILoRegclusters
+  tsne <- MigrObj@dimreductions[["S"]][[paste("CH1_15", "_TSNE")]]
+  
+  colors <- colorRampPalette(c("gold","royalblue","forestgreen","darkmagenta"))(length(unique(clusters)))
+  plot(tsne[,1], tsne[,2], bty="n", pch=19, cex=0.1, col=colors[clusters], axes=FALSE, xlab="", ylab="")
+  
 }
