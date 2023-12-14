cda3ef3604feed7052ff46cc938a971e5bed22b1
Markus Linden <markus.linden@utu.fi>
2023-12-14 09:54:24 +0200

    Renamed variables

--- R/violinPlot.R
+++ R/violinPlot.R
@@ -2,13 +2,12 @@
 
 plot_violin <- function(MigrObj, dat.slot = NULL, feature = NULL, type = NULL, clusterType = "ILoRegclusters") {
   violin_data <- data.frame(
-    cluster = clusterings(MigrObj)[[type]][[clusterType]],
-    feature = getdt(MigrObj, dat.slot, type)[[feature]]
-
-  )
+    clusters = clusterings(MigrObj)[[type]][[clusterType]],
+    features = getdt(MigrObj, dat.slot, type)[[feature]]
+ )
   
   
-  ggplot(violin_data, aes(x=cluster, y=feature, fill=cluster)) +
+  ggplot(violin_data, aes(x=clusters, y=features, fill=clusters)) +
     geom_violin() + theme_classic() + labs(y=feature)
   
   
@@ -17,13 +16,13 @@ plot_violin <- function(MigrObj, dat.slot = NULL, feature = NULL, type = NULL, c
 # Plots violin plot of features grouped by clusters horizontally
 plot_hviolin <- function(MigrObj, dat.slot = NULL, feature = NULL, type = NULL, clusterType = "ILoRegclusters") {
   violin_data <- data.frame(
-    cluster = clusterings(MigrObj)[[type]][[clusterType]],
-    feature = getdt(MigrObj, dat.slot, type)[[feature]]
+    clusters = clusterings(MigrObj)[[type]][[clusterType]],
+    features = getdt(MigrObj, dat.slot, type)[[feature]]
     
   )
   
   
-  ggplot(violin_data, aes(x=cluster, y=feature, fill=cluster)) +
+  ggplot(violin_data, aes(x=clusters, y=features, fill=clusters)) +
     geom_violin() + theme_classic() + coord_flip() + labs(y=feature)
   
   
