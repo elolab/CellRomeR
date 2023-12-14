a9a421773df751c869dca7c0180194372849fa88
Tomi Suomi <tomi.suomi@utu.fi>
2023-12-14 15:05:52 +0200

    Add t-SNE legend

--- R/plot_tsne.R
+++ R/plot_tsne.R
@@ -10,4 +10,6 @@ plot_tsne <- function(MigrObj, uniq=NULL) {
   colors <- colorRampPalette(c("gold","royalblue","forestgreen","darkmagenta"))(length(unique(clusters)))
   plot(tsne[,1], tsne[,2], bty="n", pch=19, cex=0.1, col=colors[clusters], axes=FALSE, xlab="", ylab="")
   
+  legend("bottom", legend=unique(clusters), pch=21, ncol=ceiling(length(unique(clusters))/2), pt.bg=colors, bty="n")
+  
 }
