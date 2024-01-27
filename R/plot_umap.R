cc718abf092bb216a3b46743486bd11baddde49b
Tomi Suomi <tomi.suomi@utu.fi>
2024-01-27 18:37:08 +0200

    Add option to show variables with UMAP

--- R/plot_umap.R
+++ R/plot_umap.R
@@ -1,4 +1,4 @@
-plot_umap <- function(MigrObj, uniq="ILoReg", color.by="ILoRegclusters", spots.slot="raw", point.size=1) {
+plot_umap <- function(MigrObj, uniq="ILoReg", color.by="ILoRegclusters", spots.slot="raw", cex=0.1) {
   
   if (length(unique(MigrObj@clustering$S$ILoRegclusters))==1) {
     stop("No clustering data available. Please run the clustering first.")
@@ -8,12 +8,24 @@ plot_umap <- function(MigrObj, uniq="ILoReg", color.by="ILoRegclusters", spots.s
     stop("Please specify a named clustering")
   }
   
-  clusters <- clusterings(MigrObj)[["S"]]$ILoRegclusters
-  tsne <- MigrObj@dimreductions[["S"]][[paste0(uniq, "_UMAP")]]
+  umap <- MigrObj@dimreductions[["S"]][[paste0(uniq, "_UMAP")]]
   
-  colors <- colorRampPalette(c("#3C95AC","#E756A7","#FFCB8A"))(length(unique(clusters)))
-  plot(tsne[,1], tsne[,2], bty="n", pch=19, cex=0.1, col=colors[clusters], axes=FALSE, xlab="", ylab="")
-  
-  legend("bottom", legend=unique(clusters), pch=21, ncol=ceiling(length(unique(clusters))/2), pt.bg=colors, bty="n")
-  
+  if (color.by=="ILoRegclusters") {
+    clusters <- clusterings(MigrObj)[["S"]]$ILoRegclusters
+    colors <- colorRampPalette(c("#3C95AC","#E756A7","#FFCB8A"))(length(unique(clusters)))
+    plot(umap[,1], umap[,2], bty="n", pch=19, cex=cex, col=colors[clusters], axes=FALSE, xlab="", ylab="")
+    legend("bottom", legend=unique(clusters), pch=21, ncol=ceiling(length(unique(clusters))/2), pt.bg=colors, bty="n")
+  } else if (color.by %in% colnames(MigrObj@spots[[spots.slot]])) {
+    values <- MigrObj@spots[[spots.slot]][[color.by]]
+    var.order <- findInterval(values, sort(values))
+    colors <- colorRampPalette(c("#FCB317", "white", "#AB1F91"))(length(values))
+    color.legend <- range(values)
+    color.legend[3] <- ((color.legend[2] - color.legend[1])/2) + color.legend[1]
+    color.legend <- round(sort(color.legend), 1)
+    col=colors[var.order]
+    plot(umap[,1], umap[,2], bty="n", pch=19, cex=cex, col=colors[var.order], axes=FALSE, xlab="", ylab="")
+  } else {
+    stop("'color.by' variable not found. Please check the variable provided.")
+  }
+
 }
