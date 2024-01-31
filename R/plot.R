172649ec98a51f6e91dc2c08b54d5cbf06d2dd40
Tomi Suomi <tomi.suomi@utu.fi>
2024-01-31 20:40:27 +0200

    Update track plotting

--- R/plot.R
+++ R/plot.R
@@ -5,7 +5,7 @@ plot.MigrDat <- function(x, ...) {
   
   x.coords <- x@spots$raw$POSITION_X
   y.coords <- x@spots$raw$POSITION_Y
-  plot(x.coords, y.coords, pch=NA, ...)
+  plot(x.coords, y.coords, pch=NA, bty="n", xaxt="n", yaxt="n", ann=FALSE, ...)
   
   ord <- order(x@spots$raw$POSITION_T, decreasing=FALSE)
   for(track in unique(x@spots$raw$TRACK_ID)) {
@@ -14,7 +14,10 @@ plot.MigrDat <- function(x, ...) {
   }
   for(track in unique(x@spots$raw$TRACK_ID)) {
     sel <- which(x@spots$raw$TRACK_ID[ord]==track)
-    points(x.coords[ord][sel], y.coords[ord][sel], pch=21, col=adjustcolor(colors[ord][sel], offset=c(-0.3, -0.3, -0.3, 0)), bg=colors[ord][sel], cex=0.5)
+    sel.a <- sel[-length(sel)]
+    sel.b <- sel[length(sel)]
+    points(x.coords[ord][sel.a], y.coords[ord][sel.a], pch=21, col=adjustcolor(colors[ord][sel.a], offset=c(-0.3, -0.3, -0.3, 0)), bg=colors[ord][sel.a], cex=0.1)
+    points(x.coords[ord][sel.b], y.coords[ord][sel.b], pch=21, col=adjustcolor(colors[ord][sel.b], offset=c(-0.3, -0.3, -0.3, 0)), bg=colors[ord][sel.b], cex=0.5)
   }
   
 }
