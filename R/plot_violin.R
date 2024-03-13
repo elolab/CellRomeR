89e02fb20ab6c43b7bb681bf455b5c266f09c66d
Tomi Suomi <tomi.suomi@utu.fi>
2024-03-13 13:31:50 +0200

    Add edges-to-spots option for violin plot

--- R/plot_violin.R
+++ R/plot_violin.R
@@ -1,6 +1,6 @@
 # Plots violin plot of features grouped by clusters
 
-plot_violin <- function(MigrObj, feature = NULL, data.slot = "raw", type = "S") {
+plot_violin <- function(MigrObj, feature = NULL, data.slot="raw", type="S") {
   
   if (length(unique(MigrObj@clustering$S$ILoRegclusters))==1) {
     stop("No clustering data available. Please run the clustering first.")
@@ -10,18 +10,24 @@ plot_violin <- function(MigrObj, feature = NULL, data.slot = "raw", type = "S") 
     stop("Please specify a feature to plot")
   }
   
-  violin_data <- data.frame(
-    cluster = clusterings(MigrObj)[[type]][["ILoRegclusters"]],
-    feature = getdt(MigrObj, data.slot, type)[[feature]]
-  )
+  if (type=="ES") {
+    cluster <- clusterings(MigrObj)[["S"]][["ILoRegclusters"]]
+    cluster.id <- getdt(MigrObj, data.slot, "S")[["SPOT_ID"]]
+    values <- getdt(MigrObj, data.slot, "E")[[feature]]
+    values.id <- getdt(MigrObj, data.slot, "E")[["SPOT_SOURCE_ID"]]
+    cluster <- cluster[match(values.id, cluster.id)]
+    violin_data <- data.frame(cluster, values)
+  } else {
+    cluster <- clusterings(MigrObj)[[type]][["ILoRegclusters"]]
+    values <- getdt(MigrObj, data.slot, type)[[feature]]
+    violin_data <- data.frame(cluster, values)
+  }
   
   colors <- colorRampPalette(c("#3C95AC", "#E756A7", "#FFCB8A"))(length(unique(violin_data$cluster)))
   
-  
-  ggplot2::ggplot(violin_data, ggplot2::aes(x=cluster, y=feature, fill=cluster)) +
+  ggplot2::ggplot(violin_data, ggplot2::aes(x=cluster, y=values, fill=cluster)) +
     ggplot2::scale_fill_manual(values=colors) + 
     ggplot2::geom_violin() + ggplot2::theme_classic() + ggplot2::labs(y=feature)
     
-  
 }
 
